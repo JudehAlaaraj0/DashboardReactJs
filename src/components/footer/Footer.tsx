948f8ed65d622e8aa2a59424f5dbcc1e1124bd55
Judeh Alaaraj <JudehAlaaraj@gmail.com>
2024-04-16 13:04:07 +0200

    First commit, done by JudehAlaaraj0

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,12 @@
+import "./Footer.scss"
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <span>lamadmin</span>
+      <span>Judeh's Cool Dashboard!</span>
+    </div>
+  )
+}
+//feet TESTING 2 TESTING 3ALSKJDFKJASLDF;JASKDFJs
+export default Footer
