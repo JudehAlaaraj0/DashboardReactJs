722b8c143159aec99c44e445a81ad4b7007ace5f
Judeh Alaaraj <JudehAlaaraj@gmail.com>
2024-04-17 16:14:42 +0200

    Finished up the users list

--- src/components/dataTable/DataTable.tsx
+++ src/components/dataTable/DataTable.tsx
@@ -1,81 +1,49 @@
 import { DataGrid, GridColDef, GridToolbar } from "@mui/x-data-grid";
 import "./dataTable.scss";
+import { Link } from "react-router-dom";
 
-const DataTable = () => {
-    const columns: GridColDef<(typeof rows)[number]>[] = [
-        { field: 'id', headerName: 'ID', width: 90 },
-        {
-            field: "avatar", headerName: "Avatar", width: 100,
-            renderCell: (params) => {
-                return(
-                    <div className="avatarContainer">
-                        <img src={params.row.img || "/noavatar.png"} alt="" />
-                    </div>
-                ) 
-            }
-        },
-        {
-            field: "actions", headerName: "Actions", width: 100,
-            renderCell: (params) => {
-                return( <div className="action">
-                    <div className="view">View</div>
-                    <div className="delete">Delete</div>
-                    </div>
-                ) 
-            }
-        },
-        {
-            field: "status", 
-            headerName: "Status", 
-            width: 100, 
-            type:"boolean"
-        },
-        {
-            field: 'firstName',
-            headerName: 'First name',
-            width: 150,
-            editable: true,
-        },
-        {
-            field: 'lastName',
-            headerName: 'Last name',
-            width: 150,
-            editable: true,
-        },
-        {
-            field: 'age',
-            headerName: 'Age',
-            type: 'number',
-            width: 110,
-            editable: true,
-        },
-        {
-            field: 'fullName',
-            headerName: 'Full name',
-            description: 'This column has a value getter and is not sortable.',
-            sortable: false,
-            width: 160,
-            valueGetter: (value, row) => `${row.firstName || ''} ${row.lastName || ''}`,
-        },
-    ];
 
-    const rows = [
-        { id: 1, lastName: 'Snow', firstName: 'Jon', age: 14, status: true},
-        { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 31 },
-        { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 31 },
-        { id: 4, lastName: 'Stark', firstName: 'Arya', age: 11 },
-        { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-        { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-        { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-        { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-        { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-    ];
-    return (
-        <div className="dataTable">
+type Props = {
+    columns: GridColDef[];
+    rows: object[];
+    slug: string;
+}
+
+const DataTable = (props: Props) => {
+
+
+
+    const handleDelete = (id:number) => {
+        //deleting the item!
+        console.log(id + " has been deleted.");
+    }
+
+        const actionColumn: GridColDef = {
+            field: "actions",
+            headerName: "Actions",
+            width: 100,
+            renderCell: (params) => {
+                return (
+                <div className="action">
+                    <Link to={`/${props.slug}/${params.row.id}`}>
+                        <img src="/view.svg" alt="" />
+                    </Link>
+                    <div className="delete" onClick={()=>handleDelete(params.row.id)}>
+                        <img src="/delete.svg" alt="" />
+                    </div>
+                    {/* <div className="view">View</div>
+                    <div className="delete">Delete</div> */}
+                </div>
+                )
+            }
+        }
+
+        return (
+            <div className="dataTable">
             <DataGrid
                 className="dataGrid"
-                rows={rows}
-                columns={columns}
+                rows={props.rows}
+                columns={[...props.columns, actionColumn]}
                 initialState={{
                     pagination: {
                         paginationModel: {
@@ -96,9 +64,9 @@ const DataTable = () => {
                 disableColumnFilter
                 disableDensitySelector
                 disableColumnSelector
-            />
-        </div>
-    )
+                />
+            </div>
+        )
 }
 
 export default DataTable
--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -1,5 +1,57 @@
+import { GridColDef } from "@mui/x-data-grid";
 import DataTable from "../../components/dataTable/DataTable";
 import "./Users.scss";
+import { userRows } from "../../data";
+
+const columns: GridColDef[] = [
+  { field: 'id', headerName: 'ID', width: 90 },
+  {
+    field: "avatar", headerName: "Avatar", width: 100,
+    renderCell: (params) => {
+      return (
+        <div className="avatarContainer">
+          <img src={params.row.img || "/noavatar.png"} alt="" />
+        </div>
+      )
+    }
+  },
+  {
+    field: 'firstName',
+    headerName: 'First name',
+    width: 150,
+    type: "string",
+  },
+  {
+    field: 'lastName',
+    headerName: 'Last name',
+    width: 150,
+    type: "string",
+  },
+  {
+    field: 'email',
+    headerName: 'Email',
+    width: 200,
+    type: "string",
+  },
+  {
+    field: 'phone',
+    headerName: 'Phone',
+    width: 200,
+    type: "string",
+  },
+  {
+    field: 'createdAt',
+    headerName: 'Created At',
+    width: 100,
+  },
+  {
+    field: 'verified',
+    headerName: 'Verified',
+    width: 100,
+    type: "boolean",
+  },
+];
+
 
 const Users = () => {
   return (
@@ -8,7 +60,7 @@ const Users = () => {
         <h1>Users</h1>
         <button>Add new User</button>
       </div>
-      <DataTable />
+      <DataTable slug="users" columns={columns} rows={userRows}/>
     </div>
   )
 }
