1d0aef6dc6b3e593ccf8c30328b3e7e245210ec5
Judeh Alaaraj <JudehAlaaraj@gmail.com>
2024-04-23 01:53:29 +0200

    Finished!

--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -1,7 +1,6 @@
 import { GridColDef } from "@mui/x-data-grid";
 import DataTable from "../../components/dataTable/DataTable";
 import "./Users.scss";
-import { userRows } from "../../data";
 import { useState } from "react";
 import Add from "../../components/add/Add";
 import { useQuery } from "@tanstack/react-query";
